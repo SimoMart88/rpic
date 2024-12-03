d8857cf2c8082187c177e880d930b124b4467403
SimoMart88 <simone.martinotti@gmail.com>
2024-12-03 21:33:38 +0100

    feat(Device): implement Device model

--- src/rpi_controller/core/models.py
+++ src/rpi_controller/core/models.py
@@ -1,2 +1,26 @@
-
 # Create your models here.
+
+from django.db import models
+from strategy_field.fields import StrategyField
+
+from typing import Any
+
+
+class Device(models.Model):
+    _registry = None
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    visible = models.BooleanField(default=True)
+    enabled = models.BooleanField(default=True)
+    config = models.JSONField(default=dict)
+    interface = StrategyField(registry=lambda model: model._registry)
+
+    def use(self, *args: Any, **kwargs: Any) -> dict[Any, Any]:
+        raise NotImplementedError
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        abstract = True
