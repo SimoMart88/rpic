51cecc42c560983fa84432601aa3df8e81aa0b34
SimoMart88 <simone.martinotti@gmail.com>
2025-05-12 21:10:42 +0200

    feat(Tasks): implement support for Actuator update_status async execution

--- src/rpi_controller/tasks.py
+++ src/rpi_controller/tasks.py
@@ -1,15 +1,24 @@
+import typing
 import logging
 
 from rpi_controller.config.celery import app as celery_app
-from rpi_controller.models import Sensor
+from rpi_controller.models import Sensor, Actuator
 
 
 logger = logging.getLogger(__name__)
 
 
 @celery_app.task(bind=True)
-def sensor_read_status(self: celery_app, sensor_slug: int) -> None:
+def sensor_read_status(self: celery_app, sensor_slug: str) -> None:
     logger.info("[Sensor '%s'] Read status Celery task: %s", sensor_slug, self.request.id)
 
     sensor = Sensor.objects.get(slug=sensor_slug)
     sensor.read_status()
+
+
+@celery_app.task(bind=True)
+def actuator_update_status(self: celery_app, actuator_slug: str, *args: typing.Any, **kwargs: typing.Any) -> None:
+    logger.info("[Actuator '%s'] Update status Celery task: %s", actuator_slug, self.request.id)
+
+    actuator = Actuator.objects.get(slug=actuator_slug)
+    actuator.update_status(*args, **kwargs)
--- tests/unit/test_tasks.py
+++ tests/unit/test_tasks.py
@@ -1,20 +1,27 @@
 import pytest
 import typing
 
-from rpi_controller.tasks import sensor_read_status
+from rpi_controller.tasks import sensor_read_status, actuator_update_status
 
 
 if typing.TYPE_CHECKING:
-    from rpi_controller.models import Sensor
+    from rpi_controller.models import Device
+    from _pytest.fixtures import TopRequest
 
 
 @pytest.mark.django_db()
-def test_sensor_read_status(dummy_sensor: "Sensor") -> None:
-    assert not dummy_sensor.last_status_update_time  # Sanity check
-    assert dummy_sensor.last_update_status == dummy_sensor.UpdateStatus.NEW  # Sanity check
+@pytest.mark.parametrize("device_fixture_name,task_to_run", [
+    pytest.param("dummy_sensor", sensor_read_status, id="sensor"),
+    pytest.param("dummy_actuator", actuator_update_status, id="actuator"),
+])
+def test_task(device_fixture_name: str, task_to_run: typing.Callable[[str], None], request: "TopRequest") -> None:
+    dummy_device: "Device" = request.getfixturevalue(device_fixture_name)
 
-    sensor_read_status(dummy_sensor.slug)
+    assert not dummy_device.last_status_update_time  # Sanity check
+    assert dummy_device.last_update_status == dummy_device.UpdateStatus.NEW  # Sanity check
 
-    dummy_sensor.refresh_from_db()
-    assert dummy_sensor.last_status_update_time
-    assert dummy_sensor.last_update_status == dummy_sensor.UpdateStatus.SUCCESS
+    task_to_run(dummy_device.slug)
+
+    dummy_device.refresh_from_db()
+    assert dummy_device.last_status_update_time
+    assert dummy_device.last_update_status == dummy_device.UpdateStatus.SUCCESS
