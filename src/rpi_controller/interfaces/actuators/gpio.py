0d6383f7fc14324c1766766d3777318b782def25
SimoMart88 <simone.martinotti@gmail.com>
2025-07-20 21:41:08 +0200

    feat(lock_db_record): implement decorator to lock at db record level when a operation is performed on GPIO

--- src/rpi_controller/interfaces/actuators/gpio.py
+++ src/rpi_controller/interfaces/actuators/gpio.py
@@ -6,9 +6,10 @@ from rpi_controller.interfaces.forms import ConfigForm
 from rpi_controller.interfaces.actuators.base import ActuatorInterface
 from rpi_controller.interfaces.exceptions import (InterfaceRuntimeException,
                                                   InterfaceUserInputException)
+from rpi_controller.interfaces.utils import is_status_update_required
 from rpi_controller.interfaces.utils.gpio import get_gpio_pin_from_config
 from rpi_controller.interfaces.actuators.hardware.relay import RelayContactsState, create_hardware_interface
-
+from rpi_controller.interfaces.utils.locks import lock_db_record
 
 if typing.TYPE_CHECKING:
     from rpi_controller.interfaces.actuators.hardware.relay import IRelay
@@ -22,6 +23,7 @@ class RelayInterfaceForm(ConfigForm):
     contacts_state = forms.ChoiceField(
         choices=RelayContactsState.choices(), initial=RelayContactsState.NORMALLY_OPEN,
     )
+    refresh_min_interval = forms.IntegerField(label="Actuator refresh minimum interval (in seconds)", min_value=5)
 
 
 
@@ -31,7 +33,12 @@ class RelayActuatorInterface(ActuatorInterface):
     config_form = RelayInterfaceForm
     template_name = "rpi_controller/interfaces/actuators/relay.html"
 
+    @lock_db_record
     def read_input(self) -> dict[str, typing.Any]:
+        is_status_update_required(
+            self.context, self.context.last_status_update_time, self.context.config.get("refresh_min_interval", 60)
+        )
+
         gpio_pin = get_gpio_pin_from_config(self.context.config)
         contacts_state = self.context.config.get("contacts_state", RelayContactsState.NORMALLY_OPEN)
 
@@ -49,7 +56,7 @@ class RelayActuatorInterface(ActuatorInterface):
         finally:
             actuator.close()
 
-
+    @lock_db_record
     def control(self, *args: typing.Any, **kwargs: typing.Any) -> dict[typing.Any, typing.Any]:
         try:
             active = kwargs["active"]
--- src/rpi_controller/interfaces/sensors/gpio.py
+++ src/rpi_controller/interfaces/sensors/gpio.py
@@ -1,6 +1,5 @@
 import typing
 import logging
-from django.db import transaction
 from django import forms
 
 from rpi_controller.interfaces.forms import ConfigForm
@@ -9,7 +8,7 @@ from rpi_controller.interfaces.exceptions import InterfaceRuntimeException
 from rpi_controller.interfaces.utils import is_status_update_required
 from rpi_controller.interfaces.utils.gpio import get_gpio_pin_from_config
 from rpi_controller.interfaces.sensors.hardware.dht22 import create_hardware_interface
-
+from rpi_controller.interfaces.utils.locks import lock_db_record
 
 logger = logging.getLogger(__name__)
 
@@ -30,26 +29,23 @@ class Dht22SensorInterface(SensorInterface):
     config_form = Dht22SensorInterfaceForm
     template_name = "rpi_controller/interfaces/sensors/dht22.html"
 
+    @lock_db_record
     def read_input(self) -> dict[str, typing.Any]:
         is_status_update_required(
             self.context, self.context.last_status_update_time, self.context.config.get("refresh_min_interval", 60)
         )
 
-        with transaction.atomic():
-            # Lock model object to avoid concurrent read on the same sensor
-            context = type(self.context).objects.select_for_update().get(id=self.context.id)
+        gpio_pin = get_gpio_pin_from_config(self.context.config)
+        retries = self.context.config.get("retry_number", 5)
 
-            gpio_pin = get_gpio_pin_from_config(context.config)
-            retries = context.config.get("retry_number", 5)
+        try:
+            sensor: "IDHT22" = create_hardware_interface(gpio_pin)
+            temperature, humidity = sensor.read(retries=retries)
+        except Exception as e:
+            logger.error("[Sensor '%s'] Unexpected error: %s", self.context.slug, e)
+            raise InterfaceRuntimeException('DHT22 sensor unexpected error') from e
 
-            try:
-                sensor: "IDHT22" = create_hardware_interface(gpio_pin)
-                temperature, humidity = sensor.read(retries=retries)
-            except Exception as e:
-                logger.error("[Sensor '%s'] Unexpected error: %s", context.slug, e)
-                raise InterfaceRuntimeException('DHT22 sensor unexpected error') from e
-
-            if humidity is not None and temperature is not None:
-                return {'temperature': round(temperature, 1), 'humidity': round(humidity, 1)}
-            else:
-                raise InterfaceRuntimeException('DHT22 sensor read failure, temperature/humidity not available')
+        if humidity is not None and temperature is not None:
+            return {'temperature': round(temperature, 1), 'humidity': round(humidity, 1)}
+        else:
+            raise InterfaceRuntimeException('DHT22 sensor read failure, temperature/humidity not available')
